63deec59be08c14f07fe3a36cb16447d63ef9a11
Madjid Sahki <b_atmans@hotmail.fr>
2021-01-27 16:08:36 +0100

    test front

--- ITI.Poll.Spa/src/app/poll.service.spec.ts
+++ ITI.Poll.Spa/src/app/poll.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { of } from "rxjs";
 import { GraphQLResult, GraphQLService } from "./graph-ql.service";
-import { MyPollResult, PollService } from "./poll.service";
+import { MyPollResult, PollService, CreatePollResult, PollPayload, PollInput } from "./poll.service";
 
 describe('PollService', () => {
   let graphQLServiceSpy: jasmine.SpyObj<GraphQLService>;
@@ -61,6 +61,41 @@ describe('PollService', () => {
       .subscribe(result => {
         expect(result).toBe(me);
       });
+  });
+
+  it('can add my poll', ()=> {
+    const poll: PollPayload = {
+      errors : [],
+      poll : {
+        pollId: 1,
+        question: "question",
+        answers: [],
+        guestCount: 3
+      }
+    }
+
+    var o: PollInput = {
+      question: "question",
+      proposals: ["pop1","prop2"],
+      guests: ["g1","g2"]
+    }
+
+    const pollResult: GraphQLResult<CreatePollResult> = {
+      errors:[],
+      data:{
+        poll: {
+          createPoll: poll
+        }
+      }
+    }
+
+    const graphQLServiceSpy: jasmine.SpyObj<GraphQLService> =
+    jasmine.createSpyObj<GraphQLService>('GraphQLService', ['secureSend']);
+    graphQLServiceSpy.secureSend.and.returnValue(of(pollResult));
+    const sut = new PollService(graphQLServiceSpy);
+
+    sut.createPoll(o).subscribe(result =>{expect(result).toBe(pollResult)})
+
   });
 
   it('calls secureSend correctly', () => {
